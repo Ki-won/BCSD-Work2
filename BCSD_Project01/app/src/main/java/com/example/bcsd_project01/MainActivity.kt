3a24b4eb83821b361303d4b446661c134fb86c7d
seogiwon <seokw2240@gmail.com>
2021-11-23 11:17:59 +0900

    make fragment ?

--- BCSD_Project01/app/src/main/java/com/example/bcsd_project01/MainActivity.kt
+++ BCSD_Project01/app/src/main/java/com/example/bcsd_project01/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.bcsd_project01
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -9,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,27 +17,32 @@ class MainActivity : AppCompatActivity() {
         val toast = findViewById<Button>(R.id.button_toast)
         val count = findViewById<Button>(R.id.button_count)
         val random = findViewById<Button>(R.id.button_random)
-        val sentertxet = findViewById<TextView>(R.id.sentertxet)
+
+        val senterTxet = findViewById<TextView>(R.id.sentertxet)
         var number = 0
 
 
-        toast.setOnClickListener(){
-        Toast.makeText(this@MainActivity, " toast", Toast.LENGTH_SHORT).show()
+
+        toast.setOnClickListener() {
+            Toast.makeText(this@MainActivity, " toast", Toast.LENGTH_SHORT).show()
         }
 
-        count.setOnClickListener(){
-        number++
-            sentertxet.text = number.toString()
+        count.setOnClickListener() {
+            number++
+            senterTxet.text = number.toString()
         }
 
-        random.setOnClickListener(){
-            val intent = Intent(this, secondActivity::class.java)
-            intent.putExtra("main_num",number)
-            startActivity(intent)
+        random.setOnClickListener() {
+           showCutFrag()
         }
 
     }
 
-
-
+    fun showCutFrag(){
+        val transaction = manager.beginTransaction()
+        val frament = cutFragment()
+        transaction.replace(R.id.frameLayoutMainAt, frament)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
--- BCSD_Project01/app/src/main/java/com/example/bcsd_project01/cutFragment.kt
+++ BCSD_Project01/app/src/main/java/com/example/bcsd_project01/cutFragment.kt
@@ -6,54 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [cutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class cutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cut, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment cutFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            cutFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
     }
 }
+/*
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_second)
+
+    var number = intent.getIntExtra("main_num",-1) //* 메인 엑티비에서 가져온 카운트 횟수
+
+    val show_num =findViewById<TextView>(R.id.textView2)// 문장 텍스트 뷰의 주소 값
+    val main_text =findViewById<TextView>(R.id.maintext)//랜덤된 숫자를 표시하는 텍스트 뷰의 주소 값
+
+    var random = SecureRandom()//랜덤난수 생성
+    var random_number = random.nextInt(number)//랜덤된 숫자를 얻음 0~ number
+
+    var improtstr1 = getString(R.string.mainstr, number)
+
+    show_num.text = improtstr1//변할 텍스트에 두개의 문자열을 합쳐서 넣는다.
+    main_text.text = random_number.toString()//랜덤으로 얻은 숫자를 문자열로 변환해서 넣기
+
+ */
