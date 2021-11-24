54cf10eefd13f0c4ff63cf6215a0c9c17c873e70
seogiwon <seokw2240@gmail.com>
2021-11-24 16:47:16 +0900

    add diralog

---
+++ BCSD_Project01/app/src/main/java/com/example/bcsd_project01/CutFrag.kt
@@ -0,0 +1,36 @@
+package com.example.bcsd_project01
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.security.SecureRandom
+
+class CutFrag : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?
+                              , savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_cut, container, false)
+        val show_num = v.findViewById<TextView>(R.id.fragment_sohw_couningText)// 문장 텍스트 뷰의 주소 값
+        val main_text = v.findViewById<TextView>(R.id.fragment_show_stringText)//랜덤된 숫자를 표시하는 텍스트 뷰의 주소 값
+
+        val improtstr1 = getString(R.string.CutFrag_strText, 6)
+        var random_number: Int? = 0
+
+        show_num.text = improtstr1//변할 텍스트에 두개의 문자열을 합쳐서 넣는다.
+        main_text.text = random_number.toString()//랜덤으로 얻은 숫자를 문자열로 변환해서 넣기
+        val random = SecureRandom()//랜덤난수 생성
+
+        random_number = random.nextInt(6)//랜덤된 숫자를 얻음 0~ number
+
+        return v
+    }
+
+}
--- BCSD_Project01/app/src/main/java/com/example/bcsd_project01/MainActivity.kt
+++ BCSD_Project01/app/src/main/java/com/example/bcsd_project01/MainActivity.kt
@@ -1,47 +1,66 @@
 package com.example.bcsd_project01
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
+    private val fragmentManager = supportFragmentManager
+    private lateinit var transaction: FragmentTransaction
+
+    //메인엑티비티 내에서 가변되는 카운팅 변수
+    var countNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toast = findViewById<Button>(R.id.button_toast)
-        val count = findViewById<Button>(R.id.button_count)
-        val random = findViewById<Button>(R.id.button_random)
-
-        val senterTxet = findViewById<TextView>(R.id.sentertxet)
-        var number = 0
-
-
-
-        toast.setOnClickListener() {
-            Toast.makeText(this@MainActivity, " toast", Toast.LENGTH_SHORT).show()
-        }
-
-        count.setOnClickListener() {
-            number++
-            senterTxet.text = number.toString()
-        }
-
-        random.setOnClickListener() {
-           showCutFrag(cutFragment())
-        }
-
-    }
-
-    fun showCutFrag(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frameLayoutMainAt, fragment)
-        transaction.addToBackStack(null)
+        transaction= fragmentManager.beginTransaction()
+        transaction.add<CutFrag>(R.id.frame_Layout_Main)
         transaction.commit()
+
+        //mainActivity의 레이아웃주소들
+        val toastBtnId = findViewById<Button>(R.id.button_toast)
+        val countBtnId = findViewById<Button>(R.id.button_count)
+        val randomBtnId = findViewById<Button>(R.id.button_random)
+        val countingText = findViewById<TextView>(R.id.show_counting_Text)
+
+
+
+        //각 버튼별 기능
+        toastBtnId.setOnClickListener {
+            // 다이얼로그 추가
+            val getDlg: AlertDialog.Builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
+            getDlg.setTitle("다이얼로그활성화")
+            getDlg.setMessage("하나를 선택해주세요.\n 카운트초기화 \n toast출력 \n 종료")
+            getDlg.setPositiveButton("초기화", DialogInterface.OnClickListener {dialog, which ->
+                countNumber = 0
+            })
+            getDlg.setNeutralButton("출력", DialogInterface.OnClickListener {dialog, which ->
+                Toast.makeText(this@MainActivity, " 토스트먹고싶다", Toast.LENGTH_SHORT).show()
+            })
+
+            getDlg.show()
+        }
+
+        countBtnId.setOnClickListener {
+            countNumber++
+            countingText.text = countNumber.toString()
+        }
+
+        randomBtnId.setOnClickListener {
+        transaction.replace(R.id.frame_Layout_Main, CutFrag())
+        transaction.commit()
+        }
+
+        fun driverNumber(): Int{
+            return countNumber
+        }
     }
 }
